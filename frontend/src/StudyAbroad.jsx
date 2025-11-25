e6fe147e9a17f6543bed2a5c506f9c2a94c957ed
Bhaskar Reddy <164631537+Bhas90@users.noreply.github.com>
2025-11-25 16:05:43 +0530

    Update StudyAbroad.jsx

--- frontend/src/StudyAbroad.jsx
+++ frontend/src/StudyAbroad.jsx
@@ -8,7 +8,7 @@ const StudyAbroad = () => {
       <div className="w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         {/* Title */}
         <h2 className="text-3xl md:text-4xl font-bold text-gray-900">
-        About <span style={{ color: '#00b4e6' }}>Developer</span>
+        About <span style={{ color: '#fea611' }}>Developer</span>
         </h2>
 
         {/* Main Content */}
